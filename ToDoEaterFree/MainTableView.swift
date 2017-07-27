36df08b652afc537a47590b8aeef7ad6897bd85b
Paweł Liczmański <paweliczek@gmail.com>
2017-07-28 00:01:44 +0200

    ok mam następujące problemy: 1. na głównym ekranie pomimo zarejestrowania cell tabela nie chce wyswietlić tej cellki :( 2. w SettingsTableVC jest funkcja że przy switchu robi ViewWillApear(true) - mówiłeś że tak nie powinno być 3. SettingsTVC po użyciu switch nie odświerza się kolor tabeli 4. Po użyciu switch nie odświerza się kolor UISegmentControlera, który na MainVC wyświetla sie po kliknięciu dolnej belki HideBarView

---
+++ ToDoEaterFree/MainTableView.swift
@@ -0,0 +1,21 @@
+//
+//  MainTableView.swift
+//  ToDoEaterFree
+//
+//  Created by Paweł Liczmański on 27.07.2017.
+//  Copyright © 2017 Paweł Liczmański. All rights reserved.
+//
+
+import UIKit
+
+class MainTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ ToDoEaterFree/MainTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainTableViewCell.swift
+//  ToDoEaterFree
+//
+//  Created by Paweł Liczmański on 27.07.2017.
+//  Copyright © 2017 Paweł Liczmański. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
